613df85e018eef56d9e73f2e5dc1a654ac213453
guamy540 <49961360+guamy540@users.noreply.github.com>
2023-02-09 13:08:26 -0800

    edited footer and header

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,17 +6,11 @@ import {MdCopyright} from 'react-icons/md'
 
 const Footer = () => {
   return (
-    <footer className='FooterContainer flex flex-row text-white justify-between items-center'>
-      <div className='flex flex-row pl-4'>
-        <FaFacebook className='mr-2'/>
-        <FiInstagram/>
-      </div>
+    <footer className='FooterContainer flex flex-row text-white justify-center items-center'>
       <div className='flex flex-col items-center justify-center pt-2 pb-2'>
         <p className='text-xs'>Copyright © 2023 Task Remodeling, all rights reserved </p>
         <p className='text-xs'> Mt. Vernon, WA 98273</p>
       </div>
-      {/*The placeholder is used for centering purposes*/}
-      <div className='placeholder'></div>
     </footer>
   )
 }
